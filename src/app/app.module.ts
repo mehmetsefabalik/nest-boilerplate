4aa4ff4aa6b78974b8ac0f921a1b6be850abd785
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2019-10-21 22:40:17 +0300

    app module is moved to /app directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "./config/config.module";
+import { ConfigModule } from "../config/config.module";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ConfigService } from "./config/config.service";
-import { AuthModule } from "./auth/auth.module";
+import { ConfigService } from "../config/config.service";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   imports: [
